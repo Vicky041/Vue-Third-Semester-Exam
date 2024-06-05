f33ad87cb357b18219fc6dfce480568df5abc3fc
Vicky041 <osemekev22@gmail.com>
2024-06-05 15:48:44 +0100

    My vue Repo project

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import gitHubIcon from './icons/gitHubIcon.vue';
+</script>
+
+<template>
+
+  <nav>
+    <div class="navbar">
+      <gitHubIcon class="github-icon"/>
+      <h1 class="text">Osemeke C. Victor Github Repo</h1>
+    </div>
+  </nav>
+
+</template>
+ 
+<style scoped>
+
+.navbar {
+  position:relative;
+  display: flex; 
+  align-items: center;
+  gap: 10px;
+  justify-content: center;
+  background-color: #0a5fc0;
+  text-align: center;
+  padding: 20px;
+  margin-bottom: 100px
+}
+
+.github-icon {
+  font-size: 3rem;
+}
+
+.text {
+  font-style: italic;
+  text-decoration: solid;
+}
+</style>
